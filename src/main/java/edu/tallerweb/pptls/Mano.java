eafc03aa035a5dbc5395f2144ada296324c32627
Cesar Veron <cesarveron@gmail.com>
2013-05-09 15:02:20 -0300

    Cambios en Mano.java

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -2,16 +2,43 @@ package edu.tallerweb.pptls;
 
 /**
  * Representa una de las Manos involucradas en el juego
+	Incluye la matriz con los posibles resultados
+	Filas		Jugador 1
+	Columnas	Jugador 2
+	
+				PIEDRA	SPOCK	PAPEL	LAGARTO	TIJERA
+		
+ 	PIEDRA		EMPATA	PIERDE	PIERDE	GANA	GANA
+ 	
+ 	SPOCK		GANA	EMPATA	PIERDE	PIERDE	GANA
+ 	
+ 	PAPEL		GANA	GANA	EMPATA	PIERDE	PIERDE
+ 	
+ 	LAGARTO		PIERDE	GANA	GANA	EMPATA	PIERDE
+ 	
+ 	TIJERA		PIERDE	PIERDE	GANA	GANA	EMPATA
+ 
+ 
  */
-public class Mano {
 
+public class Mano {
+	
+	private Forma forma;
+	
+	private Resultado [][] matriz = new Resultado [][]	{{Resultado.EMPATA, Resultado.PIERDE, Resultado.PIERDE,Resultado.GANA, Resultado.GANA},
+														{Resultado.GANA, Resultado.EMPATA, Resultado.PIERDE, Resultado.PIERDE, Resultado.GANA},
+														{Resultado.GANA, Resultado.GANA, Resultado.EMPATA, Resultado.PIERDE, Resultado.PIERDE},
+														{Resultado.PIERDE, Resultado.GANA, Resultado.GANA, Resultado.EMPATA, Resultado.PIERDE},
+														{Resultado.PIERDE, Resultado.PIERDE, Resultado.GANA, Resultado.GANA, Resultado.EMPATA}};
+	
 	/**
 	 * Toda Mano debe crearse con una forma dada, que será
 	 * la que determine su condición en el juego.
 	 * @param forma, la Forma que adopta la Mano.
 	 */
 	public Mano(final Forma forma) {
-		throw new RuntimeException("No implementado aún");
+		
+		this.forma = forma;
 	}
 
 	/**
@@ -21,7 +48,8 @@ public class Mano {
 	 * @return un Resultado, de acuerdo al estado del juego.
 	 */
 	public Resultado jugarCon(final Mano otra) {
-		throw new RuntimeException("No implementado aún");
-	}
-
+		
+		return matriz[this.forma.getValor()][otra.forma.getValor()];
+			
+}
 }
